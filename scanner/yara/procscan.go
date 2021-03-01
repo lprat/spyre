b265c9bf50c600b5ae479f992bec8536e761efa9
lprat <lionel.prat9@gmail.com>
2021-03-01 19:08:17 +0100

    keep error yara scan proc

--- scanner/yara/procscan.go
+++ scanner/yara/procscan.go
@@ -196,5 +196,32 @@ func (s *procScanner) ScanProc(pid int32) error {
 			"Child_Process", strings.Join(child_exe, "|"),
 		)
 	}
+	if err != nil {
+		message = fmt.Sprintf("Error yara proc scan [%v] on process: %s[%s](%s),err,exe,pathexe,username)
+		md5sum, err := hash_file_md5(pathexe)
+		if err != nil {
+		  md5sum = ""
+		}
+		report.AddProcInfo("yara_on_pid", message,
+			"rule", m.Rule,
+			"string_match", string(matched),
+			"PID", strconv.FormatInt(int64(pid), 10),
+			"PPID", ppid,
+			"Filehash", md5sum,
+			"pathexe", pathexe,
+			"cmdline", cmdline,
+			"Process", exe,
+			"username", username,
+			"real_date", strconv.FormatInt(int64(crt_time),10),
+			"Parent_pathexe", ppathexe,
+			"Parent_cmdline", pcmdline,
+			"Parent_Process", pexe,
+			"Parent_username", pusername,
+			"Child_cmdline", strings.Join(child_cmdline, "|"),
+			"Child_pathexe", strings.Join(child_pathexe, "|"),
+			"Child_username", strings.Join(child_username, "|"),
+			"Child_Process", strings.Join(child_exe, "|"),
+		)
+	}
 	return err
 }
